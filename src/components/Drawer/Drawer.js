9bdbcacfbe48cb65ef147a4b53ba2ffc578fcf25
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-03-16 15:53:53 +0600

    added nav and logo

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,4 +1,4 @@
-import classes from "./Drower.module.css";
+import classes from "./Drawer.module.css";
 
 const Drawer = () => {
     return (
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import classes from "./Layout.module.css";
 import Drawer from "../Drawer/Drawer";
 import Toolbar from "../Toolbar/Toolbar";
-
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
 
 const Layout = ( {children}) => {
     return ( 
         <div className={classes.Layout}>
         <Toolbar/>
         <Drawer/>
+        <Logo/>
+        <Nav/>
         {children}
         </div>
      );
