a6461ff5773c0eaa299ac67591b9db8c8027d295
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-03-18 14:15:43 +0600

    add alt in img

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import logo from "../../images/bonsai.svg"
 const Logo = () => {
     return (
         <div className={classes.Logo}>
-            <img src={logo}/>
+            <img src={logo} alt="SEEDLINGS"/>
             <span>Seedlings</span>
         </div>
       );
