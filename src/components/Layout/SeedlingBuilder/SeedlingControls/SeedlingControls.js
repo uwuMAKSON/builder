63cf9c95a573b8293d96d6e775a9ab0d4c435fe3
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-03-23 14:05:36 +0600

    added ingreadient in controls

--- src/components/Layout/SeedlingBuilder/SeedlingControls/SeedlingControls.js
+++ src/components/Layout/SeedlingBuilder/SeedlingControls/SeedlingControls.js
@@ -3,24 +3,14 @@ import classes from "./SeedlingConrols.module.css";
 const SeedlingControls = () => {
   return (
     <div className={classes.SeedlingControls}>
-      <p>Apple + 0 -</p>
-       
-      <p>Apple + 0 -</p>
-      
-      <p>Apple + 0 -</p>
-      
-      <p>Apple + 0 -</p>
-     
-      <p>Apple + 0 -</p>
-      
-      <p>Apple + 0 -</p>
-      
-      <p>Apple + 0 -</p>
-      
-      <p>Apple + 0 -</p>
-      
-      <p>Apple + 0 -</p>
-      <br />
+      <p>Apples + 0 -</p>
+      <p>Pear + 0 -</p>
+      <p>Orange + 0 -</p>    
+      <p>Peach + 0 -</p>   
+      <p>Apricot + 0 -</p>   
+      <p>Plum + 0 -</p>    
+      <p>Cherry + 0 -</p>      
+      <p>Berries + 0 -</p>
         </div>
   );
 }
