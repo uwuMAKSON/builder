8fc98d6a20c0b6e3f0586e7a3593cca97e59815f
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-03-25 14:43:56 +0600

    changed to builder

--- src/components/Layout/FruitBuilder/FruitBuilder.js
+++ src/components/Layout/FruitBuilder/FruitBuilder.js
@@ -1,13 +1,14 @@
 import classes from "./FruitBuilder.module.css";
 import FruitPreview from "./FruitPreview/FruitPreview";
 import FruitControls from "./FruitControls/FruitControls";
+import { useState } from "react";
 
 const FruitBuilder = () => {
-  const ingredients = {
-    pear: 20,
-    apple: 20,
-    banana: 20,
-  };
+  const [ ingredients, setIngredients] = useState({
+    pear:20,
+    banana:20,
+    apple:20,
+  })
 
   return (
     <div className={classes.FruitBuilder}>
