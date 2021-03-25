3b0323502d163c6d6da35dab8b9014688bf2073b
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-03-25 14:19:39 +0600

    clear in logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,14 @@
+  
 import classes from "./Logo.module.css";
-import logo from "../../images/bonsai.svg"
+import logo from "../../images/bonsai.svg";
 
 const Logo = () => {
-    return (
-        <div className={classes.Logo}>
-            <img src={logo} alt="SEEDLINGS"/>
-            <span>Fruit Builder</span>
-        </div>
-      );
+  return (
+    <div className={classes.Logo}>
+      <img src={logo} alt="Logo of the Fruit project" />
+      <div>Fruit Builder</div>
+    </div>
+  );
 }
- 
+
 export default Logo;
