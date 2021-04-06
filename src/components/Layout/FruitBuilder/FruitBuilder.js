fb9663a7f94eed3daba5cb19bdfd01f2ca96ba60
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-06 14:34:05 +0600

    added price to Fruit

--- src/components/Layout/FruitBuilder/FruitBuilder.js
+++ src/components/Layout/FruitBuilder/FruitBuilder.js
@@ -5,19 +5,26 @@ import classes from "./FruitBuilder.module.css";
 import { useState } from "react";
 
 const FruitBuilder = () => {
+  const prices = {
+    apple: 15,
+    banana: 20,
+    pear: 10,
+    watermelon: 200,
+    grapes: 100,
+  };
   const [ingredients, setIngredients] = useState({
+    apple: 1,
     banana: 1,
     pear: 1,
-    apple: 1,
     watermelon: 1,
     grapes: 1,
-   
   });
   const [price, setPrice] = useState(150);
 
   function addIngredient(type) {
     const newIngredients = { ...ingredients };
     newIngredients[type]++;
+    setPrice(price + prices[type]);
     setIngredients(newIngredients);
   }
 
@@ -25,6 +32,7 @@ const FruitBuilder = () => {
     if (ingredients[type]) {
       const newIngredients = { ...ingredients };
       newIngredients[type]--;
+      setPrice(price - prices[type]);
       setIngredients(newIngredients);
     }
   }
