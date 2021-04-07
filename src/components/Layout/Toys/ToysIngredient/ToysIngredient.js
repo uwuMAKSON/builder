5bf4e449a80a885eb50945485d40258649173450
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-07 13:15:13 +0600

    I can't find an error

--- src/components/Layout/Toys/ToysIngredient/ToysIngredient.js
+++ src/components/Layout/Toys/ToysIngredient/ToysIngredient.js
@@ -2,19 +2,19 @@ import classes from "./ToysIngredient.module.css";
 
 
 import appleBackground from "../../../../imges/apple.svg";
-import pear from "../../../../imges/pear.svg";
+import pearBackground from "../../../../imges/pear.svg";
 import watermelonBackground from "../../../../images/watermelon.svg";
 import bananaBackground from "../../../../imges/banana.svg";
-import grapes from "../../../../imges/grapes.svg";
+import grapesBackground from "../../../../imges/grapes.svg";
 
 const ToysIngredient = ({ type ,fixed }) => {
   const types = {
   
    apple: { backgroundImage: `url(${appleBackground})`, width: "50px", height: "50px" },
-   pear: { backgroundImage: `url(${pear})`, width: "50px", height: "50px" },
+   pear: { backgroundImage: `url(${pearBackground})`, width: "50px", height: "50px" },
    banana: { backgroundImage: `url(${bananaBackground})`, width: "50px", height: "50px" },
     watermelon: { backgroundImage: `url(${watermelonBackground})`, width: "50px", height: "50px" },
-    grapes: { backgroundImage: `url(${grapes})`, width: "50px", height: "50px" },
+    grapes: { backgroundImage: `url(${grapesBackground})`, width: "50px", height: "50px" },
 
   };
  
