308e36c6f465835124e189013b1c72fee8f8857e
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-08 15:03:13 +0600

    added text shadow to all text

--- src/components/Layout/Toys/ToysControls/ToysControl/ToysControl.js
+++ src/components/Layout/Toys/ToysControls/ToysControl/ToysControl.js
@@ -1,13 +1,21 @@
 import Button from "../../../../UI/Backdrop/Button/Button";
-import ToysIngredient from "../../ToysIngredient/ToysIngredient";
+// import ToysIngredient from "../../ToysIngredient/ToysIngredient";
 import classes from "./ToysControl.module.css";
 
 const ToysControl = ({ type, add, remove }) => {
+  const name ={
+    watermelon:"watermelon",
+    pear:"pear",
+    apple:"apple",
+  grapes:"grapes",
+  bear:"bear",
+  }
   return (
     <div className={classes.ToysControl}>
  <Button onClick={() => add(type)}>+</Button>
       <div className={classes.ingredient}>
-        <ToysIngredient type={type} fixed />
+        {/* <ToysIngredient type={type} fixed /> */}
+        {name[type]}
       </div>
       <Button onClick={() => remove(type)}>-</Button>
     </div>
