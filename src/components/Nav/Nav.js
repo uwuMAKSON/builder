baaec1421cb1f97f0715bcd3361bc605ef538ee3
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-08 15:08:15 +0600

    added to logo text shadow and width

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,10 +2,10 @@ import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem"
 const Nav = () => {
     return (
-        <div className={classes.Nav}>
+        <ul className={classes.Nav}>
             <NavItem url="/" active>Builder</NavItem>
             <NavItem url="/" > Checkout</NavItem>
-        </div>
+        </ul>
       );
 }
  
