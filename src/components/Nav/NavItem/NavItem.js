1ed92098a0413f3dd3e7f788e1218f374ee74833
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-16 16:19:44 +0600

    added Nav Link

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,11 +1,11 @@
+import { NavLink } from "react-router-dom";
 import classes from "./NavItem.module.css";
 
 const NavItem = ({ children, url, active }) => {
     return ( 
         <li className={classes.NavItem}>
-            <a href={url} className={active ? classes.active : null}>
-                {children}
-            </a>
+            <NavLink to={url} activeClassName={classes.active} exact={exact}>{children}
+            </NavLink>
         </li>
      );
 }
