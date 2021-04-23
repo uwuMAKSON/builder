251e8c9a5bf6b1569f5c0ccc027a99a5b77ba787
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-23 15:16:18 +0600

    delete margin-right in preview

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,15 +1,15 @@
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
+import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
 
 const Checkout = ({ history }) => {
-  function cancelCallback() {
-    history.replace('/');
-  }
+    function cancelCallback() {
+      history.replace('/');
+    }
+    return (
+        <div>
+          <CheckoutSummary cancelCallback={cancelCallback} />
+        </div>
+        
+      );
+    }
 
-  return (
-    <div>
-      <CheckoutSummary cancelCallback={cancelCallback} />
-    </div>
-  );
-}
- 
 export default Checkout;
--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -2,25 +2,26 @@ import classes from "./CheckoutSummary.module.css";
 import ToysPreview from "../../../components/Layout/Toys/ToysPreview/ToysPreview";
 import Button from "../../UI/Backdrop/Button/Button";
 
+
 const CheckoutSummary = ({ cancelCallback }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <div>
-        <ToysPreview ingredients={{
-          bear: 1, 
-          car: 1,
-          cat: 1,
-          ball:1,
-          beanbag: 1,
-          pyramid: 1,
-        }} price={1020} />
+      <ToysPreview ingredients={{
+  bear: 1, 
+  car: 1,
+  cat: 1,
+  ball:1,
+  beanbag: 1,
+  pyramid: 1,
+}} price={1020} />
       </div>
       <div>
-        <Button>Checkout</Button>
+      <Button>Checkout</Button>
         <Button onClick={cancelCallback}>Cancel</Button>
       </div>
     </div>
   );
 }
- 
-export default CheckoutSummary;
+
+export default CheckoutSummary; 
