1ae2775999564985f1e39c0be334a8094d17046e
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-29 15:29:10 +0600

    added ingredient

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
 
 const Checkout = ({ history }) => {
@@ -13,9 +14,20 @@ const Checkout = ({ history }) => {
       name: data.get('name'),
       phone: data.get('phone'),
       address: data.get('address'),
+      ingredients: {
+        ball: 1,
+        beanbag: 1,
+        bear: 1,
+        car: 1,
+        cat: 1,
+        pyramid: 1,
+      }
     }
 
-    console.log(order)
+    axios.post('https://builder-a51d0-default-rtdb.firebaseio.com/orders.json', order)
+      .then(response => {
+        history.replace('/');
+      });
   }
 
   return (
