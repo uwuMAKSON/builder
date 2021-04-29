8a79c911de5e948fd6e679f8b955b5590b91f448
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-29 14:51:33 +0600

    changed chackout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,15 +1,30 @@
-import CheckoutSummary from "./CheckoutSummary/CheckoutSummary";
+import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
 
 const Checkout = ({ history }) => {
-    function cancelCallback() {
-      history.replace('/');
-    }
-    return (
-        <div>
-          <CheckoutSummary cancelCallback={cancelCallback} />
-        </div>
-        
-      );
+  function cancelCallback() {
+    history.replace('/');
+  }
+
+  function submitCallback(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    const order = {
+      name: data.get('name'),
+      phone: data.get('phone'),
+      address: data.get('address'),
     }
 
+    console.log(order)
+  }
+
+  return (
+    <div>
+      <CheckoutSummary
+        submitCallback={submitCallback}
+        cancelCallback={cancelCallback} />
+    </div>
+  );
+}
+ 
 export default Checkout;
