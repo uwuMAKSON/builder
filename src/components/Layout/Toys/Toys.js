e40ae5fa9c9462e0b3dcc1732a02583861b5fc4e
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-05-04 15:57:28 +0600

    added orders

--- src/components/Layout/Toys/Toys.js
+++ src/components/Layout/Toys/Toys.js
@@ -1,6 +1,5 @@
 import ToysPreview from "./ToysPreview/ToysPreview";
 import ToysControls from "./ToysControls/ToysControls";
-
 import classes from "./Toys.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -13,9 +12,7 @@ const ToysBuilder = ({ history }) => {
   const ingredients = useSelector(state => state.ingredients);
   const price = useSelector(state => state.price);
   const [ordering, setOrdering] = useState(false);
-
   // useEffect(loadDefaults, []);
-
   // function loadDefaults() {
   //   axios
   //     .get('https://builder-a51d0-default-rtdb.firebaseio.com/default.json')
@@ -28,37 +25,23 @@ const ToysBuilder = ({ history }) => {
   //       setIngredients(response.data.ingredients);
   //     });
   // }
-
   function startOrdering() {
     setOrdering(true);
   }
-
   function stopOrdering() {
     setOrdering(false);
   }
-
   function finishOrdering() {
     setOrdering(false);
     // loadDefaults();
     history.push('/checkout');
   }
-
   return (
     <div className={classes.ToysBuilder}>
-      <ToysPreview
-        ingredients={ingredients}
-        price={price} />
-      <ToysControls
-        ingredients={ingredients}
-        startOrdering={startOrdering}
-        />
-      <Modal
-        show={ordering}
-        cancel={stopOrdering}>
-          <OrderSummary
-            ingredients={ingredients}
-            price={price}
-            />
+      <ToysPreview ingredients={ingredients} price={price}/>
+      <ToysControls ingredients={ingredients} startOrdering={startOrdering}/>
+      <Modal show={ordering} cancel={stopOrdering}>
+      <OrderSummary ingredients={ingredients} price={price}/>
           <Button onClick={finishOrdering} green>Checkout</Button>
           <Button onClick={stopOrdering}>Cancel</Button>
         </Modal>
