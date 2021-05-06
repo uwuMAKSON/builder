2823deeb1831eafd89b0e96e7a2b5f3935cec3c5
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-05-06 14:01:56 +0600

    added toys builder in css and add names in controls

--- src/components/Layout/Toys/ToysControls/ToysControl/ToysControl.js
+++ src/components/Layout/Toys/ToysControls/ToysControl/ToysControl.js
@@ -1,15 +1,24 @@
-import { useDispatch } from "react-redux";
+import { useDispatch} from "react-redux";
 import Button from "../../../../UI/Backdrop/Button/Button";
 import classes from "./ToysControl.module.css";
-import Toys from "../../ToysIngredient/Toys"
+// import Toys from "../../ToysIngredient/Toys"
 
 const ToysControl = ({ type, count }) => {
 const dispatch = useDispatch();
+const names = {
+  ball: "ball",
+  beanbag: "beanbag",
+  robot:"robot",
+  bear:"bear",
+  bunny:"bunny",
+  girrafe:"girrafe",
+}
   return (
     <div className={classes.ToysControl}>
  <Button onClick={() => dispatch({type: "ADD_TOYS", toy:type })}>+</Button>
       <div className={classes.toy}>
-        <Toys type={type} fixed />
+        {/* <Toys type={type} fixed /> */}
+        {names[type]}
       </div>
       <Button onClick={() => dispatch({type: "REMOVE_TOYS", toy:type  })} disable={!count}>-</Button>
     </div>
