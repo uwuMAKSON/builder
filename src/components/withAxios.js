d0c549b2030ef9ff3b7d7466b3e0245ac1525ac7
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-05-12 12:19:30 +0600

    added withAxois components

---
+++ src/components/withAxios.js
@@ -0,0 +1,45 @@
+  
+import React, { useState, useEffect } from "react";
+import Modal from "./../UI/Backdrop/Modal/Modal";
+
+const withAxios = (WrappedComponent, axios) => {
+  return (props) => {
+    const [error, setError] = useState(null);
+
+    function hideModal() {
+      setError(false);
+    }
+
+    const requestInterceptor = axios.interceptors.request.use((request) => {
+      setError(false);
+      return request;
+    });
+    const responseInterceptor = axios.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        setError(error);
+        return Promise.reject(error);
+      }
+    );
+
+    useEffect(() => {
+      return () => {
+        axios.interceptors.request.eject(requestInterceptor);
+        axios.interceptors.response.eject(responseInterceptor);
+      };
+    }, [requestInterceptor, responseInterceptor]);
+
+    return (
+      <>
+        <Modal show={error} hideCallback={hideModal}>
+          {error ? error.message : "Unknown error"}
+        </Modal>
+        <WrappedComponent {...props} />
+      </>
+    );
+  };
+};
+
+export default withAxios;
