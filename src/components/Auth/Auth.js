ddd0d8105c1fa17c0bc396c34a2f3f1200ca16d2
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-05-28 14:05:16 +0600

    add in checkout back color and changed size text

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router";
 import { auth, start } from "../../store/actions/auth";
+import classes from "./Auth.module.css";
 
 const Auth = () => {
   const dispatch = useDispatch();
